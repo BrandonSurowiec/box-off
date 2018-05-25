b4cd9f742255a4577e8213d0a135b299014efbff
Brandon Surowiec <brandon.surowiec@gmail.com>
2018-05-24 20:57:08 -0400

    Introduce new auth.php route file.

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Settings Routes 
-Route::get('settings', 'Users\AccountController@show');
-Route::post('settings', 'Users\AccountController@update'); 
-
-// Profile Routes 
-Route::get('profile', 'ProfileController@index'); 
-Route::get('profile/{user}', 'ProfileController@show'); 
-
 // Post Routes 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/post/{post}', 'PostController@show'); 
